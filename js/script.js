9713325837bec9bc4659dfe1a4bcd1406424c0c7
Roman Golubev <gromniki@gmail.com>
2019-02-26 23:18:41 +0300

    Оживляю форму обратной связи

--- js/script.js
+++ js/script.js
@@ -1,6 +1,31 @@
 window.addEventListener('DOMContentLoaded', function() {
   'use strict';
 
+  var btnCallFeedback = document.querySelector('.js-btn-show-modal-feedback');
+  var modalFeedback = document.querySelector('.modal-feedback');
+  var btnClose = document.querySelector('.modal-close');
+
+  //var btnClose = document.querySelector('.');
+
+  function modalClose(modal) {
+    btnClose.addEventListener('click', function (evt) {
+      evt.preventDefault();
+      modal.classList.remove('modal-show');
+    });
+  }
+
+  btnCallFeedback.addEventListener('click', function (evt) {
+    evt.preventDefault();
+
+    modalFeedback.classList.add('modal-show');
+
+    modalClose(modalFeedback);
+  });
+
+
+
+
+
 
 });
 
