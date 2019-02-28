bb455accc21d0b8887d2b8f08ee2d298f04e4592
Roman Golubev <gromniki@gmail.com>
2019-02-28 17:50:36 +0300

    добавил анимацию ошибки формы

--- js/script.js
+++ js/script.js
@@ -16,6 +16,9 @@ window.addEventListener('DOMContentLoaded', function () {
   var btnAdded2Cart = document.querySelector('.btn-buy');
   var modalAdded2Cart = document.querySelector('.modal-added-product');
 
+  /* temp */
+  var modals = document.querySelectorAll('.modal');
+
   var isStorageSupport = true;
   var storage = '';
 
@@ -42,6 +45,9 @@ window.addEventListener('DOMContentLoaded', function () {
     if (!nameField.value || !emailField.value || !commentField.value) {
       evt.preventDefault();
       console.log('Нужно заполнить все поля');
+      //feedbackPopup.classList.remove("modal-error");
+      //feedbackPopup.offsetWidth = feedbackPopup.offsetWidth;
+      feedbackPopup.classList.add('modal-error');
     } else {
       if (isStorageSupport) {
         localStorage.setItem('name', nameField.value);
@@ -52,7 +58,6 @@ window.addEventListener('DOMContentLoaded', function () {
 
   btnMap.addEventListener('click', function (evt) {
     evt.preventDefault();
-
     mapPopup.classList.add('modal-show');
   });
 
@@ -60,18 +65,39 @@ window.addEventListener('DOMContentLoaded', function () {
     popupClose[i].addEventListener('click', function (evt) {
       evt.preventDefault();
       this.parentNode.classList.remove('modal-show');
+      this.parentNode.classList.remove("modal-error");
       console.log('Закрыл элемент ' + this.parentNode.className);
     });
   }
 
-  window.addEventListener('keydown', function (evt) {
+  document.addEventListener('keydown', function (evt) {
     if (evt.keyCode === 27) {
       evt.preventDefault();
       if (feedbackPopup.classList.contains('modal-show')) {
         feedbackPopup.classList.remove('modal-show');
+        feedbackPopup.classList.remove("modal-error");
       }
     }
   });
+
+  // for (var i = 0; i < modals.length; i++) {
+  //   modals[i].addEventListener('keydown', function (evt) {
+  //     if (evt.keyCode === 27) {
+  //       evt.preventDefault();
+  //
+  //
+  //       if (this.classList.contains('modal-show')) {
+  //         this.classList.remove('modal-show');
+  //         this.classList.remove("modal-error");
+  //       }
+  //
+  //       /*if (modals[i].classList.contains('modal-show')) {
+  //         modals[i].classList.remove('modal-show');
+  //         modals[i].classList.remove("modal-error");
+  //       }*/
+  //     }
+  //   });
+  // }
 
   // btnAdded2Cart.addEventListener('click', function (evt) {
   //   evt.preventDefault();
